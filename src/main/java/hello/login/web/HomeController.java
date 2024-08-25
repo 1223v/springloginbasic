661ca56e9ff1f113af1cd90d3cecb53d0ee5b75f
1223v <1223v@naver.com>
2024-08-25 21:21:40 +0900

    Feat: 로그인 처리하기 - 서블릿 HTTP 세션1

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,6 +1,7 @@
 package hello.login.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
@@ -43,20 +44,40 @@ public class HomeController {
     //     return "loginHome";
     // }
 
+    // @GetMapping("/")
+    // public String homeLoginV2(HttpServletRequest request, Model model) {
+    //
+    //
+    //     //세션 관리자에 저장된 회원 정보 조회
+    //     Member member = (Member)sessionManager.getSession(request);
+    //
+    //     // 로그인
+    //
+    //     if (member == null) {
+    //         return "home";
+    //     }
+    //
+    //     model.addAttribute("member", member);
+    //     return "loginHome";
+    // }
+
     @GetMapping("/")
-    public String homeLoginV2(HttpServletRequest request, Model model) {
+    public String homeLoginV3(HttpServletRequest request, Model model) {
 
-
-        //세션 관리자에 저장된 회원 정보 조회
-        Member member = (Member)sessionManager.getSession(request);
-
-        // 로그인
-
-        if (member == null) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
             return "home";
         }
 
-        model.addAttribute("member", member);
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 로그인
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
